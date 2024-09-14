5ed76f44cdd663c06fcc16ce9187ec532de5f6b1
minyeong <minyk0512@gmail.com>
2024-09-14 23:39:38 +0900

    feat: region 버튼 클릭시 region에 속하는 city 버튼 렌더되도록 변경, city 버튼 클릭시 좌표값 이동 및 레벨 변경

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,11 +1,15 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import regions from '../data/regions.js';
+import cities from '../data/cities.js';
 
 const { kakao } = window;
 const KOREA_LATLNG_CENTER = { lat: 36.2683, lng: 127.6358 };
 
 const Map = () => {
   const mapRef = useRef(null);
+
+  const [mode, setMode] = useState('regions');
+  const [selectedRegion, setSelectedRegion] = useState(null);
 
   useEffect(() => {
     const container = document.getElementById('map');
@@ -22,6 +26,16 @@ const Map = () => {
     const moveLatLng = new kakao.maps.LatLng(lat, lng);
     mapRef.current.setCenter(moveLatLng);
     mapRef.current.setLevel(10);
+
+    setMode('cities');
+    setSelectedRegion(region.region);
+  };
+
+  const handleSelectCity = (city) => {
+    const { lat, lng } = city;
+    const moveLatLng = new kakao.maps.LatLng(lat, lng);
+    mapRef.current.setCenter(moveLatLng);
+    mapRef.current.setLevel(5);
   };
 
   return (
@@ -30,19 +44,36 @@ const Map = () => {
         className='grid px-5 gap-x-3 py-3 gap-y-1'
         style={{ gridTemplateColumns: 'repeat(auto-fill, minmax(80px, 1fr))' }}
       >
-        {regions.map((region, index) => {
-          return (
-            <button
-              key={index}
-              className='rounded-xl border border-gray-800'
-              onClick={() => {
-                handleSelectRegion(region);
-              }}
-            >
-              {region.region}
-            </button>
-          );
-        })}
+        {mode === 'regions'
+          ? regions.map((region, index) => {
+              return (
+                <button
+                  key={index}
+                  className='rounded-xl border border-gray-800'
+                  onClick={() => {
+                    handleSelectRegion(region);
+                  }}
+                >
+                  {region.region}
+                </button>
+              );
+            })
+          : null}
+        {mode === 'cities'
+          ? cities[selectedRegion].map((city, index) => {
+              return (
+                <button
+                  key={index}
+                  className='rounded-xl border border-gray-800'
+                  onClick={() => {
+                    handleSelectCity(city);
+                  }}
+                >
+                  {city.city}
+                </button>
+              );
+            })
+          : null}
       </div>
       <div className='min-h-[calc(100vh-100px)] flex justify-center items-center'>
         <div
