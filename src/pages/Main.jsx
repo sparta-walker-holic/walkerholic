a13e3ea0e8e555f5bfab7c1ac0fa760fa218061e
김태흔 <you@example.com>
2024-09-15 14:01:57 +0900

    add : search 컨퍼넌트 생성 feat : main ui

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,35 @@
+import Layout from '../shared/layout';
+
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <>
+      <Layout />
+      <div>
+        <div className='text-blue-600'>
+          <button>지도보기</button>
+          <button>장소 추천하기</button>
+        </div>
+        <div>
+          <div>
+            <input
+              type='text'
+              placeholder='태그를 입력해주세요'
+            />
+            <button id='search'>검색</button>
+          </div>
+          <button>#차분한</button>
+          <button>#다정한</button>
+          <button>#병엽한</button>
+          <button>#민영한</button>
+          <button>#보영한</button>
+        </div>
+        <div>
+          <h2>최신장소</h2>
+          <div></div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Main;
