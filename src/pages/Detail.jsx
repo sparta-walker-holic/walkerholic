ef98def940650a5823bcf5bc6929afeb45fd1f50
boyoung-12 <boyoung182@gmail.com>
2024-09-20 08:16:46 +0900

    add: detail page에  map 띄움

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,10 +1,26 @@
 import { useParams } from 'react-router-dom';
 import mockData from '../mock';
+import { useEffect } from 'react';
 
 const Detail = () => {
   const { postId } = useParams();
   const post = mockData.posts.find((item) => item.id === postId.toString());
-  console.log(post.title);
+
+  useEffect(() => {
+    const { kakao } = window;
+    const mapContainer = document.getElementById('map');
+    const mapOptions = {
+      center: new kakao.maps.LatLng(33.450701, 126.570667), //이게 동적으로 들어오는 값
+      level: 3,
+    };
+    const map = new kakao.maps.Map(mapContainer, mapOptions);
+
+    const marker = new kakao.maps.Marker({
+      position: map.getCenter(),
+    });
+    marker.setMap(map);
+  }, []);
+
   return (
     <div className='bg-neutral-200  w-6/12 h-full flex mx-auto my-5 flex-col py-5 px-10 gap-5'>
       <div className='flex justify-between'>
@@ -26,7 +42,12 @@ const Detail = () => {
           src={post.img_url}
         />
       </div>
-      <div className='bg-neutral-500 w-full h-3/5'>지도</div>
+      <div
+        className='bg-neutral-500 w-full h-3/5'
+        id='map'
+      >
+        지도
+      </div>
     </div>
   );
 };
