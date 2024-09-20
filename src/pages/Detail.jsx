855760685323dcbd7db6c919f75a859a3d92ec97
boyoung-12 <boyoung182@gmail.com>
2024-09-20 21:36:51 +0900

    fix:detail페이지tanstack query써서 포스트가져옴

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,18 +1,26 @@
 import { useParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import useUserStore from '../stores/useUserStore';
+import { useGetPostById } from '../query/postQuery';
 
 const Detail = () => {
   const { postId } = useParams();
   const { user } = useUserStore((state) => state.user); //좋아요한 사용자 구별위해
-  const { data } = useQuery('posts');
-  const post = () => data === postId.toString();
+  const { data, isError, isSuccess } = useGetPostById(postId);
+
+  if (isSuccess) {
+    console.log(data);
+  }
+  if (isError) {
+    console.log('에러');
+  }
+  33.450701, 126.570667;
 
   useEffect(() => {
     const { kakao } = window;
     const mapContainer = document.getElementById('map');
     const mapOptions = {
-      center: new kakao.maps.LatLng(33.450701, 126.570667), //이게 동적으로 들어오는 값
+      center: new kakao.maps.LatLng(data.position.lat, data.position.lng), //이게 동적으로 들어오는 값
       level: 3,
     };
     const map = new kakao.maps.Map(mapContainer, mapOptions);
@@ -31,17 +39,14 @@ const Detail = () => {
       </div>
       <div className='flex justify-between'>
         <div className='flex flex-col w-4/6 gap-12'>
-          <h1 className='font-bold text-xl '>{post.title}</h1>
-          <p>닉네임:{post.author_nickname}</p>
-          <p>설명:{post.description}</p>
-          <p>
-            장소:{post.position.lat}
-            {post.position.lng}
-          </p>
+          <h1 className='font-bold text-xl '>{data.title}</h1>
+          <p>닉네임:{data.author_nickname}</p>
+          <p>설명:{data.description}</p>
+          <p>장소:{data.address} 이거주소</p>
         </div>
         <img
           className='bg-neutral-400 w-2/6 h-80'
-          src={post.img_url}
+          src={data.img_url}
         />
       </div>
       <div
