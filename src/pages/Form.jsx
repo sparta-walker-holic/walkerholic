86f9275268eec54b70047e456ea93de3cbe63cf3
dajung <kwonkkan@naver.com>
2024-09-22 04:49:18 +0900

    remove: 불필요한 코드 삭제

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useUserStore from '../stores/useUserStore';
 
