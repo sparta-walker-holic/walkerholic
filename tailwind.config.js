b9fc1f29d0bddffc4f986c00764883539ef2ed94
byoungyup_kim <qudduq999@daum.net>
2024-09-23 02:36:03 +0900

    회원가입 스타일 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,8 @@ export default {
         'bg-input': '#ffffff4d',
       },
       backgroundImage: {
-        'bg-walk': 'url(/src/assets/images/bg-walk.jpg)',
+        'bg-walk':
+          'url(https://images.unsplash.com/photo-1517328894681-0f5dfabd463c?q=80&w=2864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)',
       },
       boxShadow: {
         default: '0px 0px 20px rgba(0,0,0,0.75)',
